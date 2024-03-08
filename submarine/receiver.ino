488ec59be3414ae1a284b863a57229a631129868
LeonardoCBoar <leonardodicredico@gmail.com>
2024-03-08 19:23:36 -0300

    feat: updated code after underwater test

--- submarine/receiver.ino
+++ submarine/receiver.ino
@@ -1,43 +1,41 @@
-//Programa : Receptor RF com Arduino Mega 
-//Autor : Arduino e Cia
- 
-
 #include <VirtualWire.h>
- 
-byte message[VW_MAX_MESSAGE_LEN];    // Armazena as mensagens recebidas
-byte msgLength = VW_MAX_MESSAGE_LEN; // Armazena o tamanho das mensagens
 
-const int RECEIVER_PIN = 6;
-const int LED = 7;
+ 
+byte message[VW_MAX_MESSAGE_LEN];
+byte msgLength = VW_MAX_MESSAGE_LEN;
+
+const int RECEIVER_PIN = 7;
+const int LED = 6;
 
 int count = 0;
  
  
 void setup()   {
-  Serial.begin(9600);
-  pinMode(LED, OUTPUT);
-    vw_set_rx_pin(5); // Define o pino 5 do Arduino como entrada 
-//de dados do receptor
-    vw_setup(2000);             // Bits por segundo
-    vw_rx_start();              // Inicializa o receptor
- 
+	Serial.begin(9600);
+	pinMode(LED, OUTPUT);
+
+	vw_set_rx_pin(RECEIVER_PIN);
+	vw_setup(2000);
+	vw_rx_start();
 }
  
 void loop()
 {
-uint8_t message[VW_MAX_MESSAGE_LEN];    
-uint8_t msgLength = VW_MAX_MESSAGE_LEN; 
+	uint8_t message[VW_MAX_MESSAGE_LEN];    
+	uint8_t msgLength = VW_MAX_MESSAGE_LEN; 
  
-    if (vw_get_message(message, &msgLength)) // Non-blocking
-    {
-        count++;
-        digitalWrite(LED, HIGH);
-        Serial.print((String)count + " - Recebido: " );
-        for (int i = 0; i < msgLength; i++)
-       {
-          Serial.write(message[i]);
-       }
-    Serial.println();
-    }
-    digitalWrite(LED, LOW);
+	if (vw_get_message(message, &msgLength))
+	{
+		digitalWrite(LED, HIGH);
+		delay(100);
+		digitalWrite(LED, LOW);
+
+		count++;
+		Serial.print((String)count + " - Recebido: " );
+		for (int i = 0; i < msgLength; i++)
+		{
+			  Serial.write(message[i]);
+		}
+		Serial.println();
+	}
 }
