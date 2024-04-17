02491753dd9e90429b23c3d038fcb7ed08c11c4f
LeonardoCBoar <leonardodicredico@gmail.com>
2024-04-16 22:14:29 -0300

    feat: Added depth encoder control

--- submarine/submarine.ino
+++ submarine/submarine.ino
@@ -1,5 +1,6 @@
 #include <RH_ASK.h>
 #include <SPI.h> 
+#include <util/atomic.h>
 
 #include "messages.hpp"
 #include "motor_controller.hpp"
@@ -13,9 +14,16 @@ const int LED = 13;
 const int FORWARD_PIN = 2;
 const int BACKWARD_PIN = 3;
 const int SPEED_PIN = 4;
+const int DEPTH_ENCODER_A_PIN = 18;
+const int DEPTH_ENCODER_B_PIN = 19;
+
+const int DEPTH_ENCODER_LIMIT = 15;
 
 int count = 0;
 RH_ASK driver{2000, RX_PIN, 0};
+
+int target_depth_encoder_position = 0;
+volatile int current_depth_encoder_position = 0;
  
  
 void setup()   {
@@ -31,10 +39,29 @@ void setup()   {
    pinMode(BACKWARD_PIN, OUTPUT);
    pinMode(SPEED_PIN, OUTPUT);
 
+   pinMode(DEPTH_ENCODER_A_PIN, INPUT);
+   pinMode(DEPTH_ENCODER_B_PIN, INPUT);
+   attachInterrupt(digitalPinToInterrupt(DEPTH_ENCODER_A_PIN), on_depth_encoder_interrupt, RISING);
+}
+
+void on_depth_encoder_interrupt()
+{
+  const int b_input = digitalRead(DEPTH_ENCODER_B_PIN);
+  if(b_input > 0)
+  {
+    current_depth_encoder_position++;
+  }
+  else
+  {
+    current_depth_encoder_position--;
+  }
+  
 }
  
 void loop()
 {
+  Serial.print("Encoder position:" );
+  Serial.println(current_depth_encoder_position);
 	uint8_t message[MESSAGE_BUFFER_SIZE];    
 	uint8_t msgLength;
  
@@ -52,22 +79,19 @@ void loop()
 
     analogWrite(SPEED_PIN, 200);
 		if(received_command.get_command(ControlMessage::FORWARD))
-   {
+    {
       Serial.println("RX: FORWARD");
-      digitalWrite(FORWARD_PIN, LOW);
-      digitalWrite(BACKWARD_PIN, HIGH);
-   }
+      target_depth_encoder_position = DEPTH_ENCODER_LIMIT;
+    }
 		else if(received_command.get_command(ControlMessage::BACKWARD))
     {
       Serial.println("RX: BACKWARD");
-      digitalWrite(FORWARD_PIN, HIGH);
-      digitalWrite(BACKWARD_PIN, LOW);
+      target_depth_encoder_position = -DEPTH_ENCODER_LIMIT;
     }
       else
     {
       Serial.println("RX: STOP");
-      digitalWrite(FORWARD_PIN, LOW);
-      digitalWrite(BACKWARD_PIN, LOW);
+      target_depth_encoder_position = 0;
     }
 		if(received_command.get_command(ControlMessage::LEFT))
 			Serial.println("RX: LEFT");
@@ -78,4 +102,27 @@ void loop()
 		if(received_command.get_command(ControlMessage::UP))
 			Serial.println("RX: UP");
 	}
+
+  int depth_position;
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+    depth_position = current_depth_encoder_position;
+  }
+
+  if(target_depth_encoder_position > depth_position)
+  {
+    Serial.println("Moving encoder forward");
+    digitalWrite(FORWARD_PIN, LOW);
+    digitalWrite(BACKWARD_PIN, HIGH);
+  }
+  else if(target_depth_encoder_position < depth_position)
+  {
+    Serial.println("Moving encoder backward");
+    digitalWrite(FORWARD_PIN, HIGH);
+    digitalWrite(BACKWARD_PIN, LOW);
+  }
+  else
+  {
+    digitalWrite(FORWARD_PIN, LOW);
+    digitalWrite(BACKWARD_PIN, LOW);
+  }
 }
