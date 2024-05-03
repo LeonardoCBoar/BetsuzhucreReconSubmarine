3e27c9afaa8f1afa17ecbf5a92347c8aeae4556d
LeonardoCBoar <leonardodicredico@gmail.com>
2024-05-03 01:55:43 -0300

    feat: add submarine pins file and removed useless motor controller file

---
+++ submarine/pins.hpp
@@ -0,0 +1,21 @@
+
+const int RX_PIN = 7;
+const int LED = 13;
+
+const int DEPTH_MOTOR_SPEED = 230;
+const int DEPTH_FORWARD_PIN = 2;
+const int DEPTH_BACKWARD_PIN = 3;
+const int DEPTH_SPEED_PIN = 4;
+const int DEPTH_ENCODER_A_PIN = 18;
+const int DEPTH_ENCODER_B_PIN = 19;
+
+const int THRUST_MOTOR_SPEED = 255;
+const int THRUST_SPEED_PIN = 6;
+const int THRUST_FORWARD_PIN = 8;
+const int THRUST_BACKWARD_PIN = 9;
+
+const int TURN_MOTOR_SPEED = 255;
+const int TURN_SPEED_PIN = 5;
+const int TURN_RIGHT_PIN = 11;
+const int TURN_LEFT_PIN = 12;
+
